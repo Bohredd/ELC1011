0b7e37e86577c9b79673eb69aba6d3de617ef0c4
Diogo Antonio <113557144+Bohredd@users.noreply.github.com>
2023-07-07 11:22:11 -0300

    SALVO

--- trabalho2/mips1.asm
+++ trabalho2/mips1.asm
@@ -1,8 +1,8 @@
 .data
 	valor1: .word 10
 	valor2: .word 20
-	
-	
+	string: .asciiz "fim!"
+	mainS: .asciiz "main!"
 .text
 .globl main
 
@@ -16,17 +16,22 @@ main:
 	
 	jal procedimento1
 	
+	li $v0, 4
+	la $a0, mainS
+	syscall
+	
 	li $v0, 10
 	syscall
 	
 procedimento1:
+	move $s1, $ra # Salva o endereço de retorno para a main em $s0 
 	addiu $sp, $sp, -40 # Cria o vetorA[10];
 	li $t0, 0 # Variavel I
 	li $t1, 0 # Variavel ACUMULADOR
 
 	li $t2, 0 # Resultado para passar para vetorA[i]
 	loopP1:
-		mult $t0, $a1  # Multiplica X por I 
+		mult $t0, $a1  # Multiplica X por I x
 		
 		mflo $t2 # Puxa o valor da multiplicação do I * X
 		
@@ -34,10 +39,22 @@ procedimento1:
 		
 		sw $t2, 0($sp) # Salva em vetorA[i]
 		
-		lw $a3, 0($sp) # Carrega o valor salvo no vetorA[i] para o registrador $a3 
+		move $s0, $t0
 		
 		li $v0, 1
-		move $a0, $a3
+		move $a0, $t2 
+		syscall
+		
+		li $v0, 11
+		li $a0, 10
+		syscall
+		
+		jal procedimento2
+		
+		move $t0, $s0
+		
+		li $v0, 1
+		move $a0, $t0
 		syscall
 		
 		li $v0, 11
@@ -50,6 +67,21 @@ procedimento1:
 		
 		bne $t0, 10, loopP1
 		
-		jr $ra 
+		li $v0, 4
+		la $a0, string
+		syscall
 		
-
+	move $ra, $s1
+	jr $ra
+	
+procedimento2:
+	
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+	
+	jr $ra
