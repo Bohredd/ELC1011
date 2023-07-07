d58024a239243d9fa3999771108ec6bbda729b8b
Diogo Antonio <113557144+Bohredd@users.noreply.github.com>
2023-07-07 14:47:24 -0300

    Update mips1.asm

--- trabalho2/mips1.asm
+++ trabalho2/mips1.asm
@@ -3,6 +3,13 @@
 	valor2: .word 20
 	string: .asciiz "fim!"
 	mainS: .asciiz "main!"
+	
+	valorX: .asciiz "X: " 
+	valorY: .asciiz "Y: " 
+	linha: .asciiz " ------------------ "
+	
+	xmenor: .asciiz "if ( X < Y )"
+	while: .asciiz " while ( X > Y )"
 .text
 .globl main
 
@@ -41,29 +48,21 @@ procedimento1:
 		
 		move $s0, $t0
 		
-		li $v0, 1
-		move $a0, $t2 
-		syscall
-		
-		li $v0, 11
-		li $a0, 10
-		syscall
-		
 		jal procedimento2
 		
 		move $t0, $s0
 		
-		li $v0, 1
-		move $a0, $t0
-		syscall
-		
-		li $v0, 11
-		li $a0, 10
-		syscall
-		
 		addi $t0, $t0, 1 # I++
 		
 		addiu $sp, $sp, 4 # Incrementa $sp para proxima posição do vetorA[i]
+		
+		li $v0, 11
+		li $a0, 10 
+		syscall
+		
+		li $v0, 11
+		li $a0, 10 
+		syscall
 		
 		bne $t0, 10, loopP1
 		
@@ -75,6 +74,67 @@ procedimento1:
 	jr $ra
 	
 procedimento2:
+
+	la $a0, linha
+	li $v0, 4
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+	
+	move $s3, $ra
+	
+	lw $t0, valor1 
+	
+	add $t5, $zero, $t2
+	
+	jal procedimento3
+	
+	move $t4, $v0
+
+	li $v0, 11
+	li $a0, 10
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+
+	move $t2, $t5
+	
+	lw $t0, valor2
+	
+	jal procedimento3
+	
+	#add $t1, $t1, $v0	
+	#li $v0, 1
+	#move $a0, $t4
+	#syscall
+	
+	jr $s3
+	
+procedimento3:
+
+	move $s4, $ra
+
+	li $t4, 0 # Variavel tmp
+	
+	la $a0, valorX
+	li $v0, 4
+	syscall
+	
+	li $v0, 1
+	move $a0, $t2
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+	
+	la $a0, valorY
+	li $v0, 4
+	syscall
 	
 	li $v0, 1
 	move $a0, $t0
@@ -84,4 +144,53 @@ procedimento2:
 	li $a0, 10
 	syscall
 	
+	slt $t7, $t2, $t0
+	
+	beq $t7, 1, YmaiorX
+
+	li $v0, 1
+	move $a0, $t7
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+			
+	slt $t7, $t0, $t2 
+	
+	move $a0, $t7
+	
+	li $v0, 1
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+	
+	beq $t7, 1, whileXmaiorY
+	
+	jr $s4
+	
+YmaiorX:
+	la $a0, xmenor
+	li $v0, 4
+	syscall
+		
+	move $t4, $t2
+	move $t2, $t0
+	move $t0, $t4
+		
 	jr $ra
+		
+whileXmaiorY:
+
+	la $a0, while
+	li $v0, 4
+	syscall
+	
+	li $v0, 11
+	li $a0, 10
+	syscall
+		
+				
+	jr $ra 
